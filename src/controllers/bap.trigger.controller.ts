5f4626491e4be3a69b61116a2336a57a7069b61b
Sandeep Kumar <63061035+thisissandeepkumar@users.noreply.github.com>
2022-07-20 09:05:18 +0530

    BAP Client Side done

--- src/controllers/bap.trigger.controller.ts
+++ src/controllers/bap.trigger.controller.ts
@@ -2,7 +2,19 @@
 
 import { NextFunction, Request, Response } from "express";
 import { RequestActions } from "../schemas/configs/actions.app.config.schema";
+import { MQClient } from "../utils/rbtmq.utils"
 
 export const bapTriggerHandler = async (req: Request, res: Response, next: NextFunction, action: RequestActions) => {
-    
+    try {
+        new MQClient().publishMessage("outbox", req.body);
+        res.status(200).json({
+            message: {
+                ack: {
+                    status: "ACK"
+                }
+            }
+        });
+    } catch (err) {
+        next(err)
+    }
 };
--- src/routes/requests.routes.ts
+++ src/routes/requests.routes.ts
@@ -1,17 +1,41 @@
 import { Router } from "express";
 import { RequestActions } from "../schemas/configs/actions.app.config.schema";
 import { AppMode } from "../schemas/configs/app.config.schema";
+import { GatewayMode } from "../schemas/configs/gateway.app.config.schema";
 import { getConfig } from "../utils/config.utils";
 import logger from "../utils/logger.utils";
+import { jsonCompressorMiddleware } from "../middlewares/jsonParser.middleware"
+import { authValidatorMiddleware } from "../middlewares/auth.middleware"
+import { contextMiddleware } from "../middlewares/context.middleware"
+import validator from "../middlewares/validator.middleware"
 
 export const requestsRouter = Router();
 const configuredRequestActions = getConfig().app.actions.requests;
 
 const isConfigured: Map<RequestActions, boolean> = new Map<RequestActions, boolean>();
 
+let becknRoutes : String[] = [
+    "search",
+    "select",
+    "init",
+    "confirm",
+    "status",
+    "track",
+    "cancel",
+    "update",
+    "rating",
+    "support"
+]
+
 // BAP Configuration.
 if (getConfig().app.mode === AppMode.bap) {
     // All triggers for BAP are defined using request actions.
+    if(getConfig().app.gateway.mode === GatewayMode.client) {
+        // Client Mode
+        becknRoutes.forEach(route => {                                      // TODO
+            requestsRouter.post(`/on_${route}`, jsonCompressorMiddleware, contextMiddleware, validator)
+        })
+    }
 }
 
 // BPP Configuration.
