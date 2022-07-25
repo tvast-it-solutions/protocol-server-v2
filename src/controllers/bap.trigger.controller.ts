770697c8905c9763b52a7e7bb8a41af8640cddcd
AyushAgrawal25 <agrawal.ayush2500@gmail.com>
2022-07-25 23:44:18 +0530

    Settler Function

--- src/controllers/bap.trigger.controller.ts
+++ src/controllers/bap.trigger.controller.ts
@@ -12,6 +12,13 @@ import { parseRequestCache } from "../schemas/cache/request.cache.schema";
 import { GatewayUtils } from "../utils/gateway.utils";
 import { sendSyncResponses } from "../utils/syncResponses.utils";
 import { Exception, ExceptionType } from "../models/exception.model";
+import { createAuthHeaderConfig } from "../utils/auth.utils";
+import { registryLookup } from "../utils/lookup.utils";
+import { NetworkPaticipantType } from "../schemas/subscriberDetails.schema";
+import { callNetwork } from "../utils/becknRequester.utils";
+import { BecknResponse } from "../schemas/becknResponse.schema";
+import { SyncCache } from "../utils/cache/sync.cache.utils";
+import { errorCallback } from "../utils/callback.utils";
 
 export const bapClientTriggerHandler = async (req: Request, res: Response<{}, Locals>, next: NextFunction, action: RequestActions) => {
     try {
@@ -47,10 +54,74 @@ export const bapClientTriggerHandler = async (req: Request, res: Response<{}, Lo
 
 export const bapClientTriggerSettler = async (message: AmqbLib.ConsumeMessage | null) => {
     try {
+        const requestBody=JSON.parse(message?.content.toString()!);
+        const axios_config = await createAuthHeaderConfig(requestBody);
+        
+        const bpp_id= requestBody.context.bpp_id;
+        const bpp_uri=requestBody.context.bpp_uri;
+        const action=requestBody.context.action;
+
+        let response: BecknResponse|undefined;
+        if((bpp_id && bpp_uri) && (bpp_id!=='' && bpp_uri!=='')){
+            const subscribers=await registryLookup({
+                type: NetworkPaticipantType.BPP,
+                domain: requestBody.context.domain,
+                subscriber_id: bpp_id
+            });
+
+            for(let i=0; i<subscribers.length; i++){
+                subscribers[i].subscriber_url=bpp_uri;
+            }
+
+            response=await callNetwork(subscribers, requestBody, axios_config, action);
+        }
+        else{
+            const subscribers=await registryLookup({
+                type: NetworkPaticipantType.BG,
+                domain: requestBody.context.domain
+            });
+
+            response=await callNetwork(subscribers, requestBody, axios_config, action);
+        }
+
+        if((response.status==200)||(response.status==202)||(response.status==206)){
+            // Network Calls Succeeded.
+            return;
+        }
+
+        switch(getConfig().client.type){
+            case ClientConfigType.synchronous:{
+                const message_id=requestBody.context.message_id;
+                await SyncCache.getInstance().recordError(message_id, action as RequestActions, {
+                    // TODO: change this code.
+                    code: 651641,
+                    type: BecknErrorType.coreError,
+                    message: "Network Participant Request Failed...",
+                    data: [response]
+                });
+                break;
+            }
+            case ClientConfigType.messageQueue:{
+                // TODO: send it to the client-side application using message queue.
+                break;
+            }
+            case ClientConfigType.webhook:{
+                await errorCallback({
+                    // TODO: change this code.
+                    code: 651641,
+                    type: BecknErrorType.coreError,
+                    message: "Network Participant Request Failed...",
+                    data: [response]
+                });
+                break;
+            }
+        }
+
+        return;
     }
     catch (err) {
-        logger.error(err)
+        logger.error(err);
+
+        return;
     }
 }
-
-// TODO: create bapClientTriggerSettler()
